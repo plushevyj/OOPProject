a8882255b06e5ceff77d6a59404e3955455b2eea
kirill <ischencko.kir@gmail.com>
2022-03-16 11:01:30 +0300

    Редактирует метод __call__

--- bumbo/api.py
+++ bumbo/api.py
@@ -4,11 +4,9 @@ class API:
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hello, World!"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
-
 
     def handle_request(self, request):
         user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
