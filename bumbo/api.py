b4b0f0f582b21db87b02e65e0b6bdd835bf89ef9
kirill <ischencko.kir@gmail.com>
2022-03-16 10:59:13 +0300

    Создает request и response

--- bumbo/api.py
+++ bumbo/api.py
@@ -1,6 +1,10 @@
+from webob import Request, Response
+
 class API:
     def __call__(self, environ, start_response):
-        response_body = b"Hello, World!"
-        status = "200 OK"
-        start_response(status, headers=[])
-        return iter([response_body])
+        request = Request(environ)
+
+        response = Response()
+        response.text = "Hello, World!"
+
+        return response(environ, start_response)
