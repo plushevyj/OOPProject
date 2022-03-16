7475a73381a6734b377383094ac61ec2c483387c
kirill <ischencko.kir@gmail.com>
2022-03-16 11:43:00 +0300

    Добавляет поддержку обработчиков на основе классов

--- bumbo/api.py
+++ bumbo/api.py
@@ -1,5 +1,6 @@
 from webob import Request, Response
 from parse import parse
+import inspect
 
 class API:
     def __init__(self):
@@ -22,17 +23,18 @@ class API:
         response.status_code = 404
         response.text = "Not found."
 
-
     def handle_request(self, request):
         response = Response()
         handler, kwargs = self.find_handler(request_path=request.path)
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
         return response
-
-
 
     def find_handler(self, request_path):
         for path, handler in self.routes.items():
