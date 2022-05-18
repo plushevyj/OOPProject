587746ba34f46a5d8a690d56d14dbcb1f25fa9a5
kirill <ischencko.kir@gmail.com>
2022-05-18 12:30:07 +0300

    Добавляет тесты test_404_is_returned_for_nonexistent_static_file и test_assets_are_served

--- bumbo/test_bumbo.py
+++ bumbo/test_bumbo.py
@@ -2,6 +2,21 @@ import pytest
 
 from api import API
 
+FILE_DIR = "css"
+FILE_NAME = "main.css"
+FILE_CONTENTS = "body {background-color: red}"
+
+
+# helpers
+
+def _create_static(static_dir):
+    asset = static_dir.mkdir(FILE_DIR).join(FILE_NAME)
+    asset.write(FILE_CONTENTS)
+
+    return asset
+
+
+# tests
 
 @pytest.fixture
 def api():
@@ -119,3 +134,20 @@ def test_custom_exception_handler(api, client):
     response = client.get("http://testserver/")
 
     assert response.text == "AttributeErrorHappened"
+
+
+def test_404_is_returned_for_nonexistent_static_file(client):
+    assert client.get(f"http://testserver/main.css)").status_code == 404
+
+
+
+def test_assets_are_served(tmpdir_factory):
+    static_dir = tmpdir_factory.mktemp("static")
+    _create_static(static_dir)
+    api = API(static_dir=str(static_dir))
+    client = api.test_session()
+
+    response = client.get(f"http://testserver/{FILE_DIR}/{FILE_NAME}")
+
+    assert response.status_code == 200
+    assert response.text == FILE_CONTENTS
