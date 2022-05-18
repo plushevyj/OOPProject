690a7552d08bfa63b76ff37250af266352e43330
kirill <ischencko.kir@gmail.com>
2022-05-18 10:44:33 +0300

    Создает тест test_parameterized_route

--- bumbo/test_bumbo.py
+++ bumbo/test_bumbo.py
@@ -33,3 +33,12 @@ def test_bumbo_test_client_can_send_requests(api, client):
         resp.text = RESPONSE_TEXT
 
     assert client.get("http://testserver/hey").text == RESPONSE_TEXT
+
+
+def test_parameterized_route(api, client):
+    @api.route("/{name}")
+    def hello(req, resp, name):
+        resp.text = f"hey {name}"
+
+    assert client.get("http://testserver/matthew").text == "hey matthew"
+    assert client.get("http://testserver/ashley").text == "hey ashley"
