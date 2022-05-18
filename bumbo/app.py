4af7df9489d67667fd9ba1d0d949392b760235e6
kirill <ischencko.kir@gmail.com>
2022-05-18 12:25:53 +0300

    Добавляет обработчик исключений в app.py

--- bumbo/app.py
+++ bumbo/app.py
@@ -50,3 +50,14 @@ def template_handler(req, resp):
         context={"name": "Bumbo", "title": "Best Framework"}
     ).encode()
 
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+
+app.add_exception_handler(custom_exception_handler)
+
+
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
