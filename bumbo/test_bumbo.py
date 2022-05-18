f6243926f7a21b7ab6ccf0b631b36473644b6652
kirill <ischencko.kir@gmail.com>
2022-05-18 10:34:53 +0300

    Добавляет тест test_route_overlap_throws_exception

--- bumbo/test_bumbo.py
+++ bumbo/test_bumbo.py
@@ -12,3 +12,14 @@ def test_basic_route_adding(api):
     @api.route("/home")
     def home(req, resp):
         resp.text = "YOLO"
+
+
+def test_route_overlap_throws_exception(api):
+    @api.route("/home")
+    def home(req, resp):
+        resp.text = "YOLO"
+
+    with pytest.raises(AssertionError):
+        @api.route("/home")
+        def home2(req, resp):
+            resp.text = "YOLO"
