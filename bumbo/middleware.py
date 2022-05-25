9888e60ef66dbe0e86a550df12dd4827f834c639
kirill <ischencko.kir@gmail.com>
2022-05-25 09:57:31 +0300

    Добавляет классу Middleware метод __call__

--- bumbo/middleware.py
+++ bumbo/middleware.py
@@ -1,6 +1,13 @@
+from webob import Request
+
 class Middleware:
     def __init__(self, app):
         self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.app.handle_request(request)
+        return response(environ, start_response)
 
     def add(self, middleware_cls):
         self.app = middleware_cls(self.app)
