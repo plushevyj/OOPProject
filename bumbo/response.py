df24b872bd1b38e5a1c1aa67fb0ecfec752db3e7
kirill <ischencko.kir@gmail.com>
2022-05-25 10:49:07 +0300

    Создает в классе Response метод __call__

--- bumbo/response.py
+++ bumbo/response.py
@@ -1,3 +1,5 @@
+from webob import Response as WebObResponse
+
 class Response:
     def __init__(self):
         self.json = None
@@ -6,3 +8,9 @@ class Response:
         self.content_type = None
         self.body = b''
         self.status_code = 200
+
+    def __call__(self, environ, start_response):
+        response = WebObResponse(
+            body=self.body, content_type=self.content_type, status=self.status_code
+        )
+        return response(environ, start_response)
