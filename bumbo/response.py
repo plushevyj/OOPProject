75273f14ccad1a609b394b637176c97bd17380eb
kirill <ischencko.kir@gmail.com>
2022-05-25 10:48:22 +0300

    Создает класс Response

---
+++ bumbo/response.py
@@ -0,0 +1,8 @@
+class Response:
+    def __init__(self):
+        self.json = None
+        self.html = None
+        self.text = None
+        self.content_type = None
+        self.body = b''
+        self.status_code = 200
